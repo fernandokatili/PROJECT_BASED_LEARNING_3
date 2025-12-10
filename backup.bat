bb6b03a14762fff43d3c2884bdd0445d1239335c
Nando Katili <fernandosaputrakatili@gmail.com>
2025-12-11 03:43:14 +0800

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,7 +1,7 @@
 @echo off
-echo ===== proses backup dimulai
-mkdir "C:\backup\Sdates"
-xcopy "D: \MyFiles\Mata_kuliah" "C:\backup\$dates" /E /I Y
+echo ===== proses backup dimulai =====
+mkdir "C:\backup\$date$"
+xcopy "D:\MyFiles\Mata_kuliah" "C:\backup\$date$" /E /I Y
 echo ===== memverifikasi folder ====
 dir "C:\backup\$date$"
 pause
